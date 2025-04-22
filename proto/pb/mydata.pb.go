cdbe902cb0343dfe5c453825407ce48955b5fee7
Xepobopa <ismartsdn4477@gmail.com>
2025-04-22 20:56:04 +0200

    Update

--- proto/pb/mydata.pb.go
+++ proto/pb/mydata.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.36.6
 // 	protoc        (unknown)
-// source: proto/mydata.proto
+// source: mydata.proto
 
 package proto
 
@@ -36,7 +36,7 @@ type PriceUpdate struct {
 
 func (x *PriceUpdate) Reset() {
 	*x = PriceUpdate{}
-	mi := &file_proto_mydata_proto_msgTypes[0]
+	mi := &file_mydata_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -48,7 +48,7 @@ func (x *PriceUpdate) String() string {
 func (*PriceUpdate) ProtoMessage() {}
 
 func (x *PriceUpdate) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_mydata_proto_msgTypes[0]
+	mi := &file_mydata_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -61,7 +61,7 @@ func (x *PriceUpdate) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use PriceUpdate.ProtoReflect.Descriptor instead.
 func (*PriceUpdate) Descriptor() ([]byte, []int) {
-	return file_proto_mydata_proto_rawDescGZIP(), []int{0}
+	return file_mydata_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *PriceUpdate) GetPairAddress() string {
@@ -113,11 +113,11 @@ func (x *PriceUpdate) GetTimestamp() uint64 {
 	return 0
 }
 
-var File_proto_mydata_proto protoreflect.FileDescriptor
+var File_mydata_proto protoreflect.FileDescriptor
 
-const file_proto_mydata_proto_rawDesc = "" +
+const file_mydata_proto_rawDesc = "" +
 	"\n" +
-	"\x12proto/mydata.proto\x12\tmydata.v1\"\xdd\x01\n" +
+	"\fmydata.proto\x12\tmydata.v1\"\xdd\x01\n" +
 	"\vPriceUpdate\x12!\n" +
 	"\fpair_address\x18\x01 \x01(\tR\vpairAddress\x12\x16\n" +
 	"\x06token0\x18\x02 \x01(\tR\x06token0\x12\x16\n" +
@@ -129,22 +129,22 @@ const file_proto_mydata_proto_rawDesc = "" +
 	"\ttimestamp\x18\a \x01(\x04R\ttimestampB8Z6github.com/Lazy-Parser/solana-substream/proto/pb/protob\x06proto3"
 
 var (
-	file_proto_mydata_proto_rawDescOnce sync.Once
-	file_proto_mydata_proto_rawDescData []byte
+	file_mydata_proto_rawDescOnce sync.Once
+	file_mydata_proto_rawDescData []byte
 )
 
-func file_proto_mydata_proto_rawDescGZIP() []byte {
-	file_proto_mydata_proto_rawDescOnce.Do(func() {
-		file_proto_mydata_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_proto_mydata_proto_rawDesc), len(file_proto_mydata_proto_rawDesc)))
+func file_mydata_proto_rawDescGZIP() []byte {
+	file_mydata_proto_rawDescOnce.Do(func() {
+		file_mydata_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_mydata_proto_rawDesc), len(file_mydata_proto_rawDesc)))
 	})
-	return file_proto_mydata_proto_rawDescData
+	return file_mydata_proto_rawDescData
 }
 
-var file_proto_mydata_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_proto_mydata_proto_goTypes = []any{
+var file_mydata_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_mydata_proto_goTypes = []any{
 	(*PriceUpdate)(nil), // 0: mydata.v1.PriceUpdate
 }
-var file_proto_mydata_proto_depIdxs = []int32{
+var file_mydata_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -152,26 +152,26 @@ var file_proto_mydata_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_proto_mydata_proto_init() }
-func file_proto_mydata_proto_init() {
-	if File_proto_mydata_proto != nil {
+func init() { file_mydata_proto_init() }
+func file_mydata_proto_init() {
+	if File_mydata_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: unsafe.Slice(unsafe.StringData(file_proto_mydata_proto_rawDesc), len(file_proto_mydata_proto_rawDesc)),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_mydata_proto_rawDesc), len(file_mydata_proto_rawDesc)),
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_proto_mydata_proto_goTypes,
-		DependencyIndexes: file_proto_mydata_proto_depIdxs,
-		MessageInfos:      file_proto_mydata_proto_msgTypes,
+		GoTypes:           file_mydata_proto_goTypes,
+		DependencyIndexes: file_mydata_proto_depIdxs,
+		MessageInfos:      file_mydata_proto_msgTypes,
 	}.Build()
-	File_proto_mydata_proto = out.File
-	file_proto_mydata_proto_goTypes = nil
-	file_proto_mydata_proto_depIdxs = nil
+	File_mydata_proto = out.File
+	file_mydata_proto_goTypes = nil
+	file_mydata_proto_depIdxs = nil
 }
